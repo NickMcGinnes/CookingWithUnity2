e6eb476cb49313cbeab4484f35576c168f20fe37
Unknown <D11123772@mydit.ie>
2019-05-06 16:41:25 +0100

    Ep01Complete

---
+++ Assets/Episodes/Ep01SpaceInvaders/Ep01BulletAI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ep01BulletAI : MonoBehaviour
+{
+
+	public float Speed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+		Vector3 newVelocity = Vector3.zero;
+		newVelocity.y += Speed;
+		GetComponent<Rigidbody>().velocity = newVelocity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		/*
+		Vector3 newPosition = transform.position;
+		newPosition.y += Speed * Time.deltaTime;
+		transform.position = newPosition;
+		*/
+    }
+}
---
+++ Assets/SharedAssets/Scripts/DestroyOnCollide.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnCollide : MonoBehaviour
+{
+
+	public bool Gib = false;
+	public GameObject[] Gibs;
+
+	public float ExplosionForce = 100f;
+	public float SpawnRadius = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		if (Gib)
+		{
+			foreach (GameObject gib in Gibs)
+			{
+				GameObject instance = Instantiate(gib, transform.position + Random.insideUnitSphere * SpawnRadius, transform.rotation);
+				instance.GetComponent<Rigidbody>().AddExplosionForce(ExplosionForce, transform.position, SpawnRadius);
+			}
+		}
+	
+		Destroy(gameObject);
+	}
+}
