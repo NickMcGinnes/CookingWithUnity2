0f712ba2501f8a499260fe8c506c50b57e7eba86
Unknown <D11123772@mydit.ie>
2019-05-07 00:22:04 +0100

    ep02 start

---
+++ Assets/Episodes/Ep01SpaceInvaders/Ep02EnemyAI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ep02EnemyAI : MonoBehaviour
+{
+	public float Speed = 2f;
+
+	private int direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		Vector3 newPosition = transform.position;
+		newPosition.x += Speed * Time.deltaTime;
+		transform.position = newPosition;
+    }
+}
