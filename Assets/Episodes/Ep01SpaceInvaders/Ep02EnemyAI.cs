d299143d7c76d0051b071555bfd90a85c0ab5343
Unknown <D11123772@mydit.ie>
2019-05-10 00:07:56 +0100

    ep02 complete

--- Assets/Episodes/Ep01SpaceInvaders/Ep02EnemyAI.cs
+++ Assets/Episodes/Ep01SpaceInvaders/Ep02EnemyAI.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class Ep02EnemyAI : MonoBehaviour
 {
 	public float Speed = 2f;
+	public float RightEdgePadding;
+	public float LeftEdgePadding;
+	public float DropDistance;
 
-	private int direction;
+	public int direction = 1;
 
     // Start is called before the first frame update
     void Start()
@@ -18,7 +21,36 @@ public class Ep02EnemyAI : MonoBehaviour
     void Update()
     {
 		Vector3 newPosition = transform.position;
-		newPosition.x += Speed * Time.deltaTime;
+		newPosition.x += Speed * direction * Time.deltaTime;
 		transform.position = newPosition;
-    }
+
+
+
+		if (Camera.main.WorldToScreenPoint(transform.position).x > Screen.width)
+		{
+			direction = -1;
+			newPosition = transform.position;
+			float cameraObjectDifference = transform.position.z - Camera.main.transform.position.z;
+			newPosition.x = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0, cameraObjectDifference)).x;
+			newPosition.y -= DropDistance;
+			transform.position = newPosition;
+		}
+		if (Camera.main.WorldToScreenPoint(transform.position).x < 0f + LeftEdgePadding)
+		{
+			direction = 1;
+			newPosition = transform.position;
+			float cameraObjectDifference = transform.position.z - Camera.main.transform.position.z;
+			newPosition.x = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, cameraObjectDifference)).x;
+			newPosition.y -= DropDistance;
+			transform.position = newPosition;
+		}
+	}
+
+	void DropPostion(float xPos)
+	{
+		Vector3 np = transform.position;
+		np.y -= DropDistance;
+		np.x = xPos;
+		transform.position = np;
+	}
 }
