44a864370d38414bbe26af4cc7eb14a1e0e068fa
Unknown <D11123772@mydit.ie>
2019-05-21 10:05:20 +0100

    Ep04 and Ep05 complete

---
+++ Assets/Episodes/Ep04FPPuzzle/Ep05ActiveateBridge.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ep05ActiveateBridge : MonoBehaviour
+{
+	public GameObject Bridge;
+	public GameObject Key;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject == Key)
+		{
+			Bridge.SendMessage("ObjectActivate");
+		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.gameObject == Key)
+		{
+			Bridge.SendMessage("ObjectDeactivate");
+		}
+	}
+}
---
+++ Assets/Episodes/Ep04FPPuzzle/Ep05EnableCollisionAndChangeMaterial.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ep05EnableCollisionAndChangeMaterial : MonoBehaviour
+{
+	public Material ActiveMaterial;
+	private Material InactiveMaterial;
+
+
+	private void Start()
+	{
+		InactiveMaterial = GetComponent<Renderer>().material;
+	}
+
+	void ObjectActivate()
+	{
+		GetComponent<Collider>().enabled = true;
+		GetComponent<Renderer>().material = ActiveMaterial;
+	}
+
+	void ObjectDeactivate()
+	{
+		GetComponent<Collider>().enabled = false;
+		GetComponent<Renderer>().material = InactiveMaterial;
+	}
+}
