8ea2265418c8db361374caf7531968bdfbd3434c
Nick McGinnes <nickmcginnesgames@gmail.com>
2021-09-26 23:56:09 +0100

    first 20 mins of ep 8

---
+++ Assets/Episodes/Ep08TowerDefence/Ep08DetectClicksAndTouches.cs
@@ -0,0 +1,76 @@
+﻿//Copyright 2012 Maximilian Wolfgang Maroe, feel free to use or modify this script at your leisure
+//This script sets it up so that when you click or touch, it tries to find an object in the scene
+//that is in the direction of where you clicked, and sends a "Clicked()" message to the object.
+//Supports touch (And multitouch!)
+
+using UnityEngine;
+
+namespace CookingWithUnity2.Episodes.Ep08TowerDefence
+{
+    public class Ep08DetectClicksAndTouches : MonoBehaviour
+    {
+        //This variable is optional; if not set it will default to the main camera
+        //This is so that you can detect clicks/touches on a separate UI Camera
+        //This variable does NOT update in real time
+        public Camera detectionCamera;
+
+        //This variable adds a Debug.Log call to show what was touched
+        public bool debug = false;
+
+        //This is the actual camera we reference in the update loop, set in Start()
+        private Camera _camera;
+
+        void Start()
+        {
+            _camera = detectionCamera != null ? detectionCamera : Camera.main;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Ray ray;
+            RaycastHit hit;
+
+            //Left Click
+            if (Input.GetMouseButtonDown(0)) //Check to see if we've clicked
+            {
+                //Set up our ray from screen to scene
+                ray = _camera.ScreenPointToRay(Input.mousePosition);
+
+                //If we hit...
+                if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+                {
+                    //Tell the system what we clicked something if in debug
+                    if (debug)
+                    {
+                        Debug.Log("You clicked " + hit.collider.gameObject.name, hit.collider.gameObject);
+                    }
+
+                    //Run the Clicked() function on the clicked object
+                    hit.transform.gameObject.SendMessage("Clicked", hit.point, 
+                        SendMessageOptions.DontRequireReceiver);
+                }
+            }
+
+            //Right Click
+            if (Input.GetMouseButtonDown(1)) //Check to see if we've clicked
+            {
+                //Set up our ray from screen to scene
+                ray = _camera.ScreenPointToRay(Input.mousePosition);
+
+                //If we hit...
+                if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+                {
+                    //Tell the system what we clicked something if in debug
+                    if (debug)
+                    {
+                        Debug.Log("You right clicked " + hit.collider.gameObject.name, hit.collider.gameObject);
+                    }
+
+                    //Run the Clicked() function on the clicked object
+                    hit.transform.gameObject.SendMessage("RightClicked", hit.point, SendMessageOptions.DontRequireReceiver);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Episodes/Ep08TowerDefence/PathThroughObjects.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PathThroughObjects : MonoBehaviour
+{
+    public GameObject[] pathpoints;
+    public float speed;
+    private int _currentPathIndex;
+    private Vector3 _movementDirection;
+    
+    
+    
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position //where we are 
+        //pathpoints[0].transform.position  //where we are going
+        _movementDirection = (pathpoints[0].transform.position - transform.position).normalized;
+
+        transform.position += _movementDirection * (speed * Time.deltaTime);
+
+    }
+}
